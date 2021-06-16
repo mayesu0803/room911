7c4091030270f224177c53de08c1e71bfa1fa405
mayesu0803 <mayerly.suarez@correounivalle.edu.co>
2021-06-16 01:37:41 -0500

    Show date

--- resources/views/employed/index.blade.php
+++ resources/views/employed/index.blade.php
@@ -13,6 +13,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-12">
+             <strong>{{ Carbon\Carbon::now()->format('d, M Y') }}</strong> 
                 <div class="card">
                     <div class="card-header">
                         <div style="display: flex; justify-content: space-between; align-items: center;">

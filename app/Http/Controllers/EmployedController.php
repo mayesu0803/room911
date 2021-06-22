d4a950c38a7c80f32d991509a658d726608f19cd
mayesu0803 <mayerly.suarez@correounivalle.edu.co>
2021-06-22 12:13:14 -0500

    Importcsv

--- app/Http/Controllers/EmployedController.php
+++ app/Http/Controllers/EmployedController.php
@@ -168,6 +168,18 @@ class EmployedController extends Controller
         Excel::import(new EmployedsImport, $request->file('file')->store('temp'));
         return redirect()->route('employeds.index')
         ->with('success', 'Employeds created successfully.');
+        try {
+    $import->import('import-users.xlsx');
+} catch (\Maatwebsite\Excel\Validators\ValidationException $e) {
+     $failures = $e->failures();
+     
+     foreach ($failures as $failure) {
+         $failure->row(); // row that went wrong
+         $failure->attribute(); // either heading key (if using heading row concern) or column index
+         $failure->errors(); // Actual error messages from Laravel validator
+         $failure->values(); // The values of the row that has failed.
+     }
+}
 
     }
 
--- app/Imports/EmployedsImport.php
+++ app/Imports/EmployedsImport.php
@@ -20,9 +20,9 @@ class EmployedsImport implements ToModel
             'first_name'     => $row[1],
             'middle_name'    => $row[2],
             'last_name'      => $row[3],
-            'room_access'    => false,
+            'room_access'    => $row[4],
             'date_deleted'   => null,
-            'id_department'  => $row[4]
+            'id_department'  => $row[5]
 
         ]);
     }

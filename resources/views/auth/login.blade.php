217bbc897ddebcdd4510d6bd518aa0adfcce9132
mayesu0803 <mayerly.suarez@correounivalle.edu.co>
2021-08-05 22:58:00 -0500

    commit qa delete

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,13 +8,13 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div >
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo') }}</label>
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                            <div >
+                            <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -25,13 +25,11 @@
                             </div>
                         </div>
 
-                        <div >
-                            <label for="password" >{{ __('Contraseña') }}</label>
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                            <div >
-                                <input id="password" type="password" 
-                                class="form-control @error('password') is-invalid @enderror" 
-                                name="password" required autocomplete="current-password">
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -46,9 +44,8 @@
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Recordarme
-                                        ') }}
+                                    <label  for="remember">
+                                        {{ __('Remember Me') }}
                                     </label>
                                 </div>
                             </div>
@@ -56,12 +53,15 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                            <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Olvide la contraseña') }}
-                                    </a>
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
                             </div>
                         </div>
                     </form>
